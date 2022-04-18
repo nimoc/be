5fd15d7bad1c883790402b652f0b74100254c754
nimoc <nimocc@126.com>
2022-04-18 10:30:09 +0800

    testData

--- theory/stat_code/go/mkt_record/main.go
+++ theory/stat_code/go/mkt_record/main.go
@@ -12,8 +12,45 @@ import (
 func main() {
 	xerr.PrintStack(run())
 }
+
 // 如果先取消表的索引插入数据,速度会快很多
 func run() (err error) {
+	var visit uint8 = 1
+	var exposure uint8 = 2
+	testData := []struct {
+		userID uint32
+		mktID uint32
+		recordType uint8
+	}{
+		{1,101,exposure},
+		{1,101,visit},
+		
+		{2,101,exposure},
+		{2,102,exposure},
+		
+		{3,101,exposure},
+		{3,102,visit},
+		
+		{4,102,exposure},
+		{4,102,visit},
+	}
+	for _, datum := range testData {
+		err = createMKTRecord(datum.userID, datum.mktID, datum.recordType) ; if err != nil {
+		    xerr.PrintStack(err)
+		}
+	}
+	return
+}
+
+func createMKTRecord(userID uint32, mktID uint32,recordType uint8) (err error) {
+	// db
+	// redisClient
+	return
+}
+var db *sq.Database
+var redisClient red.GoRedisV8
+func init () {
+	var err error
 	ctx := context.Background()
 	coreRedis := goredisv8.NewClient(&goredisv8.Options{
 		Addr: "127.0.0.1:6379",
@@ -22,11 +59,11 @@ func run() (err error) {
 	err = coreRedis.Ping(ctx).Err();
 	if err != nil {
 		err = xerr.WrapPrefix("redis ping fail:", err)
-		return
+		panic(err)
 	}
-	redisClient := red.NewGoRedisV8(coreRedis)
+	redisClient = red.NewGoRedisV8(coreRedis)
 	// red.GET{Key: "some"}.Do(ctx, redisClient)
-	db, dbClose, err := sq.Open("mysql", sq.MysqlDataSource{
+	db, _, err = sq.Open("mysql", sq.MysqlDataSource{
 		Host:     "127.0.0.1",
 		Port:     "3306",
 		DB:       "be",
@@ -36,6 +73,4 @@ func run() (err error) {
 	if err != nil {
 		return
 	}
-	defer dbClose()
-	return
 }
